87ac13d8783fdcb73a124a97e0eb72dc8505be34
Ardian Haxha <ardian@brainpress.org>
2016-10-15 00:18:22 +0200

    We will just output the passwords

--- read.js
+++ read.js
@@ -3,16 +3,6 @@ glob = require('glob')
 
 // password locations fedora 24
 passwords = '/etc/sysconfig/network-scripts/keys*';
-ssid = '/etc/sysconfig/network-scripts/ifcfg*';
-
-glob(ssid, function(er, fiiles){
-  
-  var reg = /fcfg-(.*?)*/g;
-  var fin = String(fiiles).match(reg);
-
-  console.log(fin);
-});
-
 // read dir
 glob(passwords, function(er, files) {
   
@@ -29,7 +19,7 @@ glob(passwords, function(er, files) {
       var reg = /'(.*?)'/g;
 
       var fin = passwords.match(reg);
-      console.log(String(fin));
+      console.log(String(fin).replace(/[']+/g, ''));
 
 
     })
