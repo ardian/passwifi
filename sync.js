afe6a3175fb18317af58f7e6857b1f78e685cc34
Ardian Haxha <ardian.haxha@gmail.com>
2017-01-26 17:58:07 +0100

    print wifi name + password, need to make it look better

--- sync.js
+++ sync.js
@@ -1,13 +1,23 @@
 var fs = require('fs')
 var glob = require('glob')
 
-var passwords = '/etc/sysconfig/network-scripts/keys*'
+// Location for Wi-Fi passwords on Fedora
+var fedora_passwords = '/etc/sysconfig/network-scripts/keys*'
 
-var data = glob.sync(passwords)
-for (var i in data) {
-  console.log(data[i])
-  var fin = fs.readFileSync(data[i], 'utf8')
+// Location for Wi-Fi passwords on Ubuntu
+//var ubuntupasswd = '/etc/NetworkManager/system-connections/Prishtina Hackerspace 2'
 
-  process.stdout.write(fin)
+function readPassword(passwords) {
+  // put all password files in an array
+  var data = glob.sync(passwords)
+  // go over each file, and print the filename + the password
+  for (var i in data) {
+    // console.log(data[i])
+    var fin = fs.readFileSync(data[i], 'utf8')
+    var myHash = {[data[i]]: fin}
+    console.log(myHash)
+
+  }
 }
 
+readPassword(fedora_passwords)
