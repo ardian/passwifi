a66195598f588d5363a5ed43068022923009f226
Ardian Haxha <ardian.haxha@gmail.com>
2017-03-18 22:51:55 +0100

    work in prg for ubuntu systems

--- passwifi.js
+++ passwifi.js
@@ -5,19 +5,19 @@ var glob = require('glob')
 var clc = require('cli-color')
 
 // Location for Wi-Fi passwords on Fedora
-var fedoraPasswords = '/etc/sysconfig/network-scripts/keys*'
+//var fedoraPasswords = '/etc/sysconfig/network-scripts/keys*'
 
 // Location for Wi-Fi passwords on Ubuntu
-// var ubuntupasswd = '/etc/NetworkManager/system-connections/Prishtina Hackerspace 2'
+var ubuntupasswd = '/etc/NetworkManager/system-connections/*'
 
 function readPassword (passwords) {
   // put all password files in an array
   var data = glob.sync(passwords)
   // go over each file, and print the filename + the password
   for (var i in data) {
-    var fin = fs.readFileSync(data[i], 'utf8')
-    process.stdout.write('Wi-Fi Name: ' + clc.greenBright(data[i].match(/([A-Z])\w+/g).toString()) + ' Password: ' + clc.greenBright(fin))
+      var fin = fs.readFileSync(data[i], 'utf8')
+      console.log("OBJ" + fin);
   }
 }
 
-readPassword(fedoraPasswords)
+readPassword(ubuntupasswd)
