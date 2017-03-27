7962fb8a2384ec6877ede013a82dceebc0e532a1
Ardian Haxha <ardian.haxha@gmail.com>
2017-03-27 07:52:23 +0200

    first version for ubuntu

--- passwifi.js
+++ passwifi.js
@@ -5,18 +5,21 @@ var glob = require('glob')
 var clc = require('cli-color')
 
 // Location for Wi-Fi passwords on Fedora
-//var fedoraPasswords = '/etc/sysconfig/network-scripts/keys*'
+// var fedoraPasswords = '/etc/sysconfig/network-scripts/keys*'
 
 // Location for Wi-Fi passwords on Ubuntu
 var ubuntupasswd = '/etc/NetworkManager/system-connections/*'
 
 function readPassword (passwords) {
   // put all password files in an array
-  var data = glob.sync(passwords)
+  var passwordFiles = glob.sync(passwords)
   // go over each file, and print the filename + the password
-  for (var i in data) {
-      var fin = fs.readFileSync(data[i], 'utf8')
-      console.log("OBJ" + fin);
+  for (var i in passwordFiles) {
+    var fin = fs.readFileSync(passwordFiles[i], 'utf8')
+
+    process.stdout.write('Wi-Fi Name: ' + clc.greenBright(fin.match(/ssid\s*=\s*(.*)/g).toString()) +
+' Password: ' + clc.greenBright(fin.match(/psk\s*=\s*(.*)/g).toString()) + '\n'
+)
   }
 }
 
