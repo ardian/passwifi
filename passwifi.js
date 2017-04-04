27933f0dd6be9208155c4b6238b2c5497f30ceae
Ardian Haxha <ardian.haxha@gmail.com>
2017-04-05 00:01:29 +0200

    getos module to detect linux distro

--- passwifi.js
+++ passwifi.js
@@ -3,6 +3,15 @@
 var fs = require('fs')
 var glob = require('glob')
 var clc = require('cli-color')
+var getos = require('getos')
+
+// Check for linux distro
+
+getos(function(e,os) {
+  if(e) return console.log(e)
+  version = console.log("Your OS is:" +JSON.stringify(os))
+})
+
 
 // Location for Wi-Fi passwords on Fedora
 // var fedoraPasswords = '/etc/sysconfig/network-scripts/keys*'
